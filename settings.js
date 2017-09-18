a0fa9e45352d5abbbc4ce52104cf1b2a3369ea70
Justin Evans <justin@whoat.net>
2017-09-18 17:35:30 -0400

    clean up

--- settings.js
+++ settings.js
@@ -1,11 +1,5 @@
 const settings = {
-    youtube: {
-        /* TODO: Clear key for inclusion in Github!!! */
-        API_KEY: 'AIzaSyDFQ8f9Ij1iaWqiTY04YLNJ3Swjw32SNgE',
-        API_BASE_URL: 'https://www.googleapis.com/youtube/v3', // /search | /list | /delete | /update | /insert | etc...
-        CHANNEL_ID: 'UCRBlbJRZo5NJyaQ4gPaVArA',
-        defaultFeatureID: '6Ts9ZB3N1vw',
-
+    flickr: {
         FLICKR_SEARCH_METHOD: 'flickr.photos.search',
         FLICKR_BASE_URL: 'http://api.flickr.com/services/rest/',
         FLICKR_KEY: 'c3001732c0651103d2384e411eade33c',
@@ -15,7 +9,7 @@ const settings = {
      * Get full API URL:
      * Usage: settings.getPath('search')
      */
-    getUrl: (() => `${settings.youtube.FLICKR_BASE_URL}/?method=${settings.FLICKR_SEARCH_METHOD}`)
+    getUrl: (() => `${settings.flickr.FLICKR_BASE_URL}/?method=${settings.FLICKR_SEARCH_METHOD}`)
 };
 
 export default settings;
